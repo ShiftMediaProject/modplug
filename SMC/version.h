178aa69249e43a0b9ee28bc312a3c518fde5f157
Matt Oliver <protogonoi@gmail.com>
2014-03-10 15:07:29 +1100

    Added config.h to VS project.

---
+++ SMC/version.h
@@ -0,0 +1,9 @@
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "libmodplug 0.8.8.5"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.8.8.5"
+
+/* Version number of package */
+#define VERSION "0.8.8.5"
